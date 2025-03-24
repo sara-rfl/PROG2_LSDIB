56aeb6375b3aecfd4eb0b03f066de21fbec05857
lymt-s <up202306308@up.pt>
2025-03-24 19:10:33 +0000

    criação da classe paciente e adição dos limites dos sinais vitais de acordo com o conhecimento científico atual

---
+++ PROG2_LSDIB/src/Paciente.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Paciente extends Pessoa {
+
+    private List<Double> frequenciasCardiacas = new ArrayList<Double>();
+    private List<Double> temperaturas = new ArrayList<Double>();
+    private List<Double> saturacoesOxigenio = new ArrayList<Double>();
+
+    public Paciente(String nome, String dataDeNascimento, double altura, double peso) {
+        super(nome, dataDeNascimento, altura, peso);
+    }
+
+    public void addFrequenciaCardiaca (double frequenciaCardiaca) {
+        if(frequenciaCardiaca >= Main.FREQUENCIA_CARDIACA_MIN && frequenciaCardiaca <= Main.FREQUENCIA_CARDIACA_MAX) {
+            frequenciasCardiacas.add(frequenciaCardiaca);
+        }
+    }
+
+    public void addTemperatura (double temperatura) {
+        if(temperatura >= Main.TEMPERATURA_MIN && temperatura <= Main.TEMPERATURA_MAX) {
+            temperaturas.add(temperatura);
+        }
+    }
+
+    public void addSaturacaoOxigenio (double saturacaoOxigenio) {
+        if(saturacaoOxigenio >= Main.SATURACAO_MIN && saturacaoOxigenio <= Main.SATURACAO_MAX) {
+            saturacoesOxigenio.add(saturacaoOxigenio);
+        }
+    }
+
+    public List<Double> getFrequenciasCardiacas() {
+        return frequenciasCardiacas;
+    }
+
+    public List<Double> getTemperaturas() {
+        return temperaturas;
+    }
+
+    public List<Double> getSaturacoesOxigenio() {
+        return saturacoesOxigenio;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+
+}
