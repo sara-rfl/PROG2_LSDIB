efb16161fa9c4b54dd70b9ea4e0336e8731aa46a
celoooo-o <up202405247@up.pt>
2025-04-01 19:48:12 +0100

    Adição dos métodos de organização e visualização dos técnicos de saúde na classe Listas

--- PROG2_LSDIB/src/Listas.java
+++ PROG2_LSDIB/src/Listas.java
@@ -21,7 +21,30 @@ public class Listas {
             System.out.println(p);
         }
     }
+    //metodo que organiza os tecnicos de saude por ordem alfabetica
+    public static void ordenarTecnicos(List<TecnicoSaude> tecnicos) {
+        int n = tecnicos.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (tecnicos.get(j).getNome().compareTo(tecnicos.get(j + 1).getNome()) > 0) {
+                    // Troca os técnicos de posição
+                    TecnicoSaude temp = tecnicos.get(j);
+                    tecnicos.set(j, tecnicos.get(j + 1));
+                    tecnicos.set(j + 1, temp);
+                }
+            }
+        }
+    }
+
+    //metodo que faz a exibica, ja organizada por ordem alfabetica
+    public static void mostrarTecnicos(List<TecnicoSaude> tecnicos) {
+        for (TecnicoSaude tecnico : tecnicos) {
+            System.out.println(tecnico.getNome());
+        }
+    }
 }
+
+
 
 
 
